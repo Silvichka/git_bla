0a56568782f280fc9e524a305992748730d72e1d
silvichka <amalashka30@gmail.com>
2024-10-07 14:02:06 +0200

    Please enter the commit message for your changes. Lines starting

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+
+        Subtractor sub = new Subtractor();
+        System.out.println(sub.substract(6, 3));
+    }
+}
